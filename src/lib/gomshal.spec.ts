f9ed5704ad276dec732ebdd8bb0cefc571cd882d
Jozef Pažin <atiris@gmail.com>
2020-05-12 17:26:30 +0200

    feat: new structure of enums and exported class

--- src/lib/gomshal.spec.ts
+++ src/lib/gomshal.spec.ts
@@ -1,7 +1,6 @@
 // tslint:disable:no-expression-statement
 import test from 'ava';
-import { loginToGoogleMap } from './gomshal';
 
-test('loginToGoogleMap return json', t => {
-  t.deepEqual(loginToGoogleMap(), JSON.parse('{"location": true}'));
+test('openGoogleMapInPuppeteer', t => {
+  t.true('');
 });
